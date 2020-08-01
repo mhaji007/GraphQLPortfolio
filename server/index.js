215e9e379e0d591f9e8b4d040c202f8924b26815
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-31 22:34:11 -0400

    Integrate GraphQL

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const next = require('next')
+const express = require('express');
+const next = require('next');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -7,7 +9,32 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
+
+
+    // Construct a schema, using GRAPHQL schema language
+    // query named hello received from the client as a request
+    //
+    const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+`);
+
+// The root provides a resolver for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello World!'
+  }
+}
+
+server.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+
 
   server.all('*', (req, res) => {
     return handle(req, res)
