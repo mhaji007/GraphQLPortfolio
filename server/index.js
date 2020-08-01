e74ba0ec48aa54f94148afde90c308780e9dd876
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-31 23:47:32 -0400

    Add experimental portfolio resolvers

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const next = require('next');
-const { graphqlHTTP } = require('express-graphql');
+const express = require('express')
+const next = require('next')
+const {graphqlHTTP} = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -8,33 +8,75 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  const server = express();
 
-
-    // Construct a schema, using GRAPHQL schema language
-    // query named hello received from the client as a request
-    //
-    const schema = buildSchema(`
-    type Query {
-      hello: String
-    }
-`);
-
-// The root provides a resolver for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello World!'
-  }
+const data = {
+  portfolios: [
+    {
+      _id: '32dsa8d9',
+      title: 'Job in USA',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false
+    },
+    {
+      _id: 'fa6dsa78',
+      title: 'Job in Barcelona',
+      content: 'Good experience',
+      jobTitle: 'Developer',
+      isCurrentlyEmployed: true
+    },
+    {
+      _id: 'asd7a8d789',
+      title: 'Job in Germany',
+      content: 'Not bad',
+      jobTitle: 'Manager',
+      daysOfExperience: 30,
+      isCurrentlyEmployed: true
+    },
+  ]
 }
 
-server.use('/graphql', graphqlHTTP({
-  schema,
-  rootValue: root,
-  graphiql: true
-}));
+app.prepare().then(() => {
+  const server = express()
 
+  // Construct a schema, using GRAPHQL schema language
+  // What do you like to fetch
+  // ! means that cannot be null
+  const schema = buildSchema(`
+      type Porfolio {
+        _id: ID!
+        title: String
+        content: String
+        jobTitle: String,
+        daysOfExperience: Int,
+        isCurrentlyEmployed: Boolean
+      }
+      type Query {
+        hello: String
+        portfolio: Porfolio
+        portfolios: [Porfolio]
+      }
+  `);
 
+  // The root provides a resolver for each API endpoint
+  // Where what you like to fetch goes and gets a response from
+  const root = {
+    hello: () => {
+      return 'Hello World!'
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => {
+      return data.portfolios
+    }
+  }
+
+  server.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }));
 
   server.all('*', (req, res) => {
     return handle(req, res)
