f8adf594f9067bcbaa2e318dc2e9eb9a116a83ef
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-07-31 22:00:38 -0400

    Implement page navigation via navbar

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,33 +1,43 @@
-import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import Link from 'next/link'
+
+const AppLink = ({children, className, href}) =>
+  <Link href={href}>
+    <a className={className}>{children}</a>
+  </Link>
 
 const AppNavbar = () => {
 
   return (
     <div className="navbar-wrapper">
-        <Navbar expand="lg" className="navbar-dark fj-mw9">
-          <Navbar.Brand className="mr-3 font-weight-bold" href="#">Mehdi Hajikhani</Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse>
-            <Nav className="mr-auto">
-              <Nav.Link href="#" className="mr-3">
-                Portfolios
-              </Nav.Link>
-              <Nav.Link href="#" className="mr-3">
-                Forum
-              </Nav.Link>
-              <Nav.Link href="#" className="mr-3">
-                Cv
-              </Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#" className="mr-3">
-                Sign Up
-              </Nav.Link>
-              <Nav.Link href="#" className="mr-3 btn btn-success bg-green-2 bright">
-                Sign In
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+      <Navbar expand="lg" className="navbar-dark fj-mw9">
+        <AppLink
+          href="/"
+          className="navbar-brand mr-3 font-weight-bold">
+          Mehdi Hajikhani
+        </AppLink>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="mr-auto">
+            <AppLink href="/portfolios" className="nav-link mr-3">
+              Portfolios
+            </AppLink>
+            <AppLink href="/forum/categories" className="nav-link mr-3">
+              Forum
+            </AppLink>
+            <AppLink href="/cv" className="mr-3 nav-link">
+              Cv
+            </AppLink>
+          </Nav>
+          <Nav>
+            <AppLink href="/login" className="mr-3 nav-link">
+              Sign In
+            </AppLink>
+            <AppLink href="/register" className="mr-3 btn btn-success bg-green-2 bright">
+              Sign Up
+            </AppLink>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   )
