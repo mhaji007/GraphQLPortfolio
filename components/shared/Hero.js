bb0c7d4c17d9c39d43ad3273c434c25aa6679267
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-02 11:51:02 -0400

    Refactor server. Move types and resolvers to separate files

--- components/shared/Hero.js
+++ components/shared/Hero.js
@@ -7,7 +7,7 @@ return (
   <div className="fj-hero-wrapper row">
     <div className="hero-left col-md-6">
       <h1 className="white hero-title">Hey, I'm Mehdi. A web developer</h1>
-      <h2 className="white hero-subtitle">Join me on my journey </h2>
+      <h2 className="white hero-subtitle">A web developer</h2>
       <div className="button-container">
         <a href="" className="btn btn-main bg-blue ttu">See my work</a>
       </div>
