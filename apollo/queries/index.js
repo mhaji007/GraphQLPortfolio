73d9775e938092189f0d97ec768b725ff056c4ac
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-04 11:45:14 -0400

    Create portfolio with Apollo

--- apollo/queries/index.js
+++ apollo/queries/index.js
@@ -1,33 +1,56 @@
-import {gql} from 'apollo-boost'
+import { gql } from 'apollo-boost';
 
 export const GET_PORTFOLIO = gql`
   query Portfolio($id: ID) {
-      portfolio (id: $id) {
-        _id,
-        title,
-        company,
-        companyWebsite
-        location
-        jobTitle
-        description
-        startDate
-        endDate
-      }
+    portfolio (id: $id) {
+      _id,
+      title,
+      company,
+      companyWebsite
+      location
+      jobTitle
+      description
+      startDate
+      endDate
     }
-  `
+  }
+`
 
-  export const GET_PORTFOLIOS = gql`
-    query Portfolios {
-      portfolios {
-        _id,
-        title,
-        company,
-        companyWebsite
-        location
-        jobTitle
-        description
-        startDate
-        endDate
-      }
-    }`;
+export const GET_PORTFOLIOS = gql`
+  query Portfolios {
+    portfolios {
+      _id,
+      title,
+      company,
+      companyWebsite
+      location
+      jobTitle
+      description
+      startDate
+      endDate
+    }
+  }`;
 
+export const CREATE_PORTFOLIO = gql`
+  mutation CreatePortfolio {
+    createPortfolio(input: {
+      title: "New Job"
+      company: "New Company"
+      companyWebsite: "New Website"
+      location: "New Location"
+      jobTitle: "New Job Title"
+      description: "New Desc"
+      startDate: "12/12/2012"
+      endDate: "14/11/2013"
+    }) {
+      _id,
+      title,
+      company,
+      companyWebsite
+      location
+      jobTitle
+      description
+      startDate
+      endDate
+    }
+  }`;
