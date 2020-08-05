3f949939331da63eb769e879bf36a16072db039f
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-05 19:33:22 -0400

    Create portfolio model. Create fake db

--- server/database/index.js
+++ server/database/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const config = require('../config/dev');
 
+require('./models/portfolio');
+
+
 exports.connect = () => {
   mongoose.connect(config.DB_URI, {
     useNewUrlParser: true,
