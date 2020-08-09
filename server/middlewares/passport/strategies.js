08a8fb0dd29b5cced06ee962afe4b0583f972022
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-09 13:59:25 -0400

    Find user in database

--- server/middlewares/passport/strategies.js
+++ server/middlewares/passport/strategies.js
@@ -1,4 +1,3 @@
-
 const { Strategy } = require('passport-strategy');
 
 
@@ -20,15 +19,11 @@ class GraphqlStrategy extends Strategy {
   }
 
   authenticate(_, options) {
-    console.log('Calling authenticate in strategy!');
+    const done = (error, user, info) => {
+      if (error) { return this.error(error); }
+      if (!user) { return this.fail(401); }
 
-    // in done we will receive "error", "user", "info"
-    const done = () => {
-      if (true) {
-        // this.success('LoggedInUser');
-        this.error('Some nasty error!');
-      }
-      // if user then call "success" otherwise call "fail" or "error"
+      return this.success(user, info);
     }
 
     this.verify(options, done);
