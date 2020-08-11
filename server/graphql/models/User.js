faa8b4011b392ec95515ca5b082ab8fef6b347eb
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-08-11 17:49:00 -0400

    Validate password

--- server/graphql/models/User.js
+++ server/graphql/models/User.js
@@ -1,29 +1,63 @@
-class User {
-  constructor(model) {
-    this.Model = model;
-  }
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const Schema = mongoose.Schema;
 
-  signUp(signUpData) {
-    if (signUpData.password !== signUpData.passwordConfirmation) {
-      throw new Error('Password must be the same as confirmation password!');
-    }
+const userSchema = new Schema({
+  avatar: String,
+  email: {
+    type: String,
+    required: 'Email is required!',
+    lowercase: true,
+    index: true,
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+  },
+  name: {
+    type: String,
+    minlength: [6, 'Minimum name length is 6 characters!']
+  },
+  username: {
+    type: String,
+    required: true,
+    minlength: [6, 'Minimum username length is 6 characters!']
+  },
+  password: {
+    type: String,
+    minlength: [6, 'Minimum password length is 6 characters!'],
+    maxlength: [32, 'Maximum password length is 32 characters!'],
+    required: true
+  },
+  role: {
+    enum: ['guest', 'admin', 'instructor'],
+    type: String,
+    required: true,
+    default: 'guest'
+  },
+  info: String,
+  createdAt: { type: Date, default: Date.now }
+})
 
-    return this.Model.create(signUpData);
-  }
+userSchema.pre('save', function(next) {
+  const user = this;
 
-  async signIn(signInData, ctx) {
-    try {
-      const user = await ctx.authenticate(signInData);
-      return user;
-      
-    } catch(error) {
-      return error;
-    }
-  }
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err) }
 
-  signOut() {
-    return 'Signing Out...';
-  }
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) { return next(err) }
+
+      user.password = hash;
+      next();
+    });
+  });
+})
+
+userSchema.methods.validatePassword = function(candidatePassword, done) {
+  bcrypt.compare(candidatePassword, this.password, function(error, isSuccess) {
+    if (error) { return done(error); }
+
+    return done(null, isSuccess);
+  })
 }
 
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
